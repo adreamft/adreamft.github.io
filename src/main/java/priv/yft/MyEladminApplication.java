6eb7cddb9869c829a610ec5f21d70c3e83318ebc
yft <1577762434@qq.com>
2024-01-10 10:01:30 +0800

    初始化项目

---
+++ src/main/java/priv/yft/MyEladminApplication.java
@@ -0,0 +1,13 @@
+package priv.yft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEladminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEladminApplication.class, args);
+	}
+
+}
